5839449ea9b9a2c4f41974917df27869b31ae1a7
ADAMU MUHAMMAD MUHAMMAD <muhdadamu0008@gmail.com>
2024-06-18 21:37:19 +0100

    feat: node js

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Adamu Muhammad Muhammad <https://github.com/AdamsGeeky>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
